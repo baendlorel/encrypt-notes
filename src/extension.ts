89242665dcbdcf70e772f3e714437ec36f41407d
aldia_mechdream <futami16237@gmail.com>
2026-03-05 16:18:25 +0800

    init

---
+++ src/extension.ts
@@ -0,0 +1,6 @@
+import type vscode from 'vscode';
+
+export const activate = async (context: vscode.ExtensionContext) => {};
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export const deactivate = () => {};
